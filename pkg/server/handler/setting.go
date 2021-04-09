3d833f2d8ee559d6681c40b2ff2d62391e83ae57
wadayamada <wada0421514@gmail.com>
2021-04-09 15:17:58 +0900

    設定情報取得処理

---
+++ pkg/server/handler/setting.go
@@ -0,0 +1,21 @@
+package handler
+
+import (
+	"net/http"
+
+	"20dojo-online/pkg/constant"
+	"20dojo-online/pkg/http/response"
+)
+
+// HandleSettingGet ゲーム設定情報取得処理
+func HandleSettingGet() http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		response.Success(writer, &settingGetResponse{
+			GachaCoinConsumption: constant.GachaCoinConsumption,
+		})
+	}
+}
+
+type settingGetResponse struct {
+	GachaCoinConsumption int32 `json:"gachaCoinConsumption"`
+}
